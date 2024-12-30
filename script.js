93a7db32930fddca855ed0770c3ef4cb2ec9be06
Pedro <pedroaugustohot64@gmail.com>
2024-12-30 19:10:01 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nomesPokemon = ["Pokemon", "Chamander", "Bulbassaur"];
+let timePokemon = [
+    ["Pikachu", "M", "Level 1"],
+    ["Chamander", "F", "Level 3"]
+]
+
+
+
+console.log(timePokemon.length)
